bd03b76ab1cd1b95b12a1b623b474549a1fa327f
Ptmlety <ptmlety@gmail.com>
2024-10-11 10:25:59 -0600

    Update acuaticos.blade.php

--- acuaticos.blade.php
+++ acuaticos.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts\plantilla1') 
-
+@section('titulo')
+ Acuaticos
+@endsection
 @section('elementos')
 <div class="container-fluid">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
